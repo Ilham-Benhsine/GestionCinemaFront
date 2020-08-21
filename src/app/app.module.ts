ea2057208e83d5566669c939d068245f3f201850
Antoine Aubree <antoine.aubree@protonmail.com>
2020-08-21 10:21:43 +0200

    create Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AccueilComponent } from './accueil/accueil.component';
+import { FilmComponent } from './film/film.component';
+import { CinemaComponent } from './cinema/cinema.component';
+import { FilmCardComponent } from './shared/component/film-card/film-card.component';
+import { CinemaCardComponent } from './shared/component/cinema-card/cinema-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    FilmComponent,
+    CinemaComponent,
+    FilmCardComponent,
+    CinemaCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/component/cinema-card/cinema-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cinema-card',
+  templateUrl: './cinema-card.component.html',
+  styleUrls: ['./cinema-card.component.scss']
+})
+export class CinemaCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
