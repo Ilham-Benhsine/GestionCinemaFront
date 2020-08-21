19dfecf65fd8f44aa13990f9688137e3d05c9c4f
Antoine Aubree <antoine.aubree@protonmail.com>
2020-08-21 16:50:30 +0200

    update affichage film card

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,13 @@ import { FilmCardComponent } from './component/film-card/film-card.component';
 import { CinemaCardComponent } from './component/cinema-card/cinema-card.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [
+        CommonModule,
+        MatCardModule
+    ],
     exports: [CinemaCardComponent, FilmCardComponent],
     declarations: [CinemaCardComponent, FilmCardComponent],
     providers: [],
