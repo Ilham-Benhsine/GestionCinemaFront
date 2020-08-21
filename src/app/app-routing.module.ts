f5b211a4dd28e704d5bc1818370b0f0870fb7862
Antoine Aubree <antoine.aubree@protonmail.com>
2020-08-21 11:59:17 +0200

    add navBar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
+import { CinemaComponent } from './cinema/cinema.component';
+import { FilmComponent } from './film/film.component';
+import { AccueilComponent } from './accueil/accueil.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'accueil', component: AccueilComponent},
+  {path: 'film', component: FilmComponent},
+  {path: 'cinema', component: CinemaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
