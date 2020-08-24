9e647452ddf8da1800bef8b5bf4d2618ac439772
seb <sebastienrousse89@gmail.com>
2020-08-24 10:22:44 +0200

    commenté la partie cinema

--- src/app/cinema/cinema.component.ts
+++ src/app/cinema/cinema.component.ts
@@ -10,30 +10,25 @@ import {map, startWith} from 'rxjs/operators';
   styleUrls: ['./cinema.component.scss']
 })
 export class CinemaComponent implements OnInit {
-
+/*
   myControl = new FormControl();
   options: string[] = ['UGC Ciné Cité Les Halles', 'Mk2 Bibliothèque - Paris', 'UGC Ciné Cité Bercy - Paris', 'Gaumont Aquaboulevard - Paris', 'Mégarama - Villeneuve-la-Garenne', 'Pathé Multiplexe - Échirolles', 'Pathé Chavant - Grenoble',
   'Pathé Bellecour - Lyon', 'Ciné Liberté - Brest', 'Gaumont Multiplexe - Rennes', 'Méga CGR - Rennes', 'Cinéville - Lorient', 'Cinéville - Quimper',
   'Pathé Docks 76 - Rouen'];
-  filteredOptions: Observable<string[]>;
+  filteredOptions: Observable<string[]>;*/
 
   ngOnInit() {
-    this.filteredOptions = this.myControl.valueChanges
+    /*this.filteredOptions = this.myControl.valueChanges
       .pipe(
         startWith(''),
         map(value => this._filter(value))
-      );
+      );*/
   }
 
-  private _filter(value: string): string[] {
+  /*private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
 
     return this.options.filter(option => option.toLowerCase().includes(filterValue));
-  }
-
-
-  /*ngOnInit() {
   }*/
-
   
 }
